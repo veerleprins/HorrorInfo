5439a9093593563c41bb772c6e12d4769daa10c2
veerleprins <veerleprins@hotmail.com>
2021-02-05 12:43:39 +0100

    Removed comments

--- src/scripts/modules/fetchData.js
+++ src/scripts/modules/fetchData.js
@@ -1,5 +1,5 @@
 // This function fetches the data and returns a
-// json version of the response:
+// json version of the response :
 export const fetchData = async (endpoint) => {
   const response = await fetch(endpoint);
   return await response.json();
