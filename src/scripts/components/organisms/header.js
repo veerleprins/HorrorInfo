f7f49a330389ce87b9a629e1cab572c39ac5dc1e
veerleprins <veerleprins@hotmail.com>
2021-02-23 16:59:38 +0100

    Changed text of the title

--- src/scripts/components/organisms/header.js
+++ src/scripts/components/organisms/header.js
@@ -3,11 +3,7 @@ import { inputElement } from "../atoms/input";
 
 export const createHeader = () => {
   const header = document.createElement("header");
-  const article = inputElement(
-    "h1",
-    "Welke horror film wil je kijken?",
-    "Search movies..."
-  );
+  const article = inputElement("h1", "Welke horror film?", "Search movies...");
 
   header.appendChild(article);
   return header;
