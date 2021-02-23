0ac13affa7fdcdb780587d48722f4916ed5283f0
veerleprins <veerleprins@hotmail.com>
2021-02-23 23:26:27 +0100

    Removed unnecessary comments

--- src/scripts/config/config.js
+++ src/scripts/config/config.js
@@ -1,8 +1,6 @@
 export const URL = `https://api.themoviedb.org/3/`;
-export const linkYouTube = `https://www.youtube.com/watch?v=`;
-export const KEY = `?api_key=04d41953aef99908d20a6535b916898e`;
-export const lang = `&language=en-US`;
 export const posterPath = `https://image.tmdb.org/t/p/w342/`;
-
-// https://api.themoviedb.org/3/movie/723072/videos?api_key=04d41953aef99908d20a6535b916898e&language=en-US
-// key for the youtube link
+export const linkYouTube = `https://www.youtube.com/embed/`;
+export const KEY = `?api_key=04d41953aef99908d20a6535b916898e`;
+export const param = `&with_genres=27&without_genres=35%2C14%2C18%2C28&page=`;
+export const lang = `&language=en-US`;
