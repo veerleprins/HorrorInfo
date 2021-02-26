9b6bc6f2117f5efb3ef1c6811803a5ad6950955c
veerleprins <veerleprins@hotmail.com>
2021-02-26 01:17:50 +0100

    Changed function name & moved variable to config file

--- src/scripts/modules/discoveryData.js
+++ src/scripts/modules/discoveryData.js
@@ -1,14 +1,16 @@
+// Constants
+import { param } from "../config/config";
+
 // Modules
 import { fetchData } from "./collecting/collect";
 import { getNested } from "./cleaning/clean";
 import { getNecessary, sortArray } from "./cleaning/clean";
 
-export const getDiscovery = async () => {
+export const getDiscoveryMovies = async () => {
   // This function fetches a similar endpoint five times using
   // a Promise.all and cleans the data so that it can be sent
   // back to the home view:
   try {
-    const param = `&with_genres=27&without_genres=35%2C14%2C18%2C28&page=`;
     const [data1, data2, data3, data4, data5] = await Promise.all(
       [1, 2, 3, 4, 5].map((page) =>
         fetchData("discover/movie", `${param}${page}`)
