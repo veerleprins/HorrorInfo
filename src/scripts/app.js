dbcedbc66e5b99c61e3371c6731cffe2ba2d2d45
veerleprins <veerleprins@hotmail.com>
2021-02-26 21:24:23 +0100

    Changed router file into app.js file

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,29 @@
-// Routing
-import { Router } from "./routes/router";
+// Externals
+import Navigo from "navigo";
 
-const init = () => {
-  Router();
+// Routes
+import { homePage, moviePage, errorPage } from "./views/exports";
+
+export const init = () => {
+  // This function initializes the router and calls
+  // a specific function associated with the page that
+  // the user is requesting:
+  try {
+    const router = new Navigo("/", false);
+    const body = document.querySelector("body");
+    router
+      .on({
+        "/": homePage(body),
+        "/movie/:id": moviePage(body),
+        "/error": errorPage(body),
+      })
+      .notFound(() => {
+        router.navigate("/error");
+      })
+      .resolve();
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 init();
