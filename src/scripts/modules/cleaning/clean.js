f2ee3ad6944df386d69f9e998fca0298831f7687
veerleprins <veerleprins@hotmail.com>
2021-02-26 01:09:12 +0100

    Added some new functional functions to clean data

--- src/scripts/modules/cleaning/clean.js
+++ src/scripts/modules/cleaning/clean.js
@@ -1,16 +1,3 @@
-export const getNecessary = (dataArray) => {
-  // Changes the names and returns only necessary items.
-  return dataArray.map((item) => {
-    return {
-      id: item.id,
-      title: item.original_title,
-      poster: item.poster_path,
-      avgVoted: item.vote_average,
-      voted: item.vote_count,
-    };
-  });
-};
-
 export const sortArray = (dataArray, item) => {
   // Sorts an array from the highest value to the lowest
   // value according to a specific item in the array.
@@ -28,4 +15,51 @@ export const getNested = (dataArray) => {
     });
   });
   return newDataArray;
+};
+
+export const getList = (dataObject, keyArray, itemName) => {
+  // This function goes through the 'keyArray' and checks if
+  // the key exists in the 'dataObject'. If it exists, it loops
+  // through all the values of the key and pushes a specific
+  // item of the value to a new array:
+  const newArray = [];
+  keyArray.forEach((key) => {
+    if (key in dataObject) {
+      dataObject[key].forEach((value) => {
+        newArray.push(value[itemName]);
+      });
+    }
+  });
+  return newArray;
+};
+
+export const getUnique = (dataArray) => {
+  // This function filters the unique value from
+  // the dataArray and returns it as a new array:
+  // Source: https://attacomsian.com/blog/javascript-remove-duplicates-from-array
+  return dataArray.filter((value, index) => {
+    return dataArray.indexOf(value) === index;
+  });
+};
+
+export const getReplaced = (dataArray, value) => {
+  // This function changes all spaces in the 'dataArray'
+  // to the new 'item', changes everything to lowercase,
+  // and returns the new array:
+  return dataArray.map((item) => {
+    return item.replace(/\s/g, value).toLowerCase();
+  });
+};
+
+export const getNecessary = (dataArray) => {
+  // Changes the names and returns only necessary items.
+  return dataArray.map((item) => {
+    return {
+      id: item.id,
+      title: item.original_title,
+      poster: item.poster_path,
+      avgVoted: item.vote_average,
+      voted: item.vote_count,
+    };
+  });
 };
