1036329f4c1110692d29dd0ee9717f0e3960758f
veerleprins <veerleprins@hotmail.com>
2021-02-26 21:31:47 +0100

    Changed some function names

--- src/scripts/components/molecules/img-link.js
+++ src/scripts/components/molecules/img-link.js
@@ -1,10 +1,9 @@
 // Components:
-import { imgElement } from "../atoms/img";
+import { createElement } from "../atoms/element";
 
-export const imgLink = (url, imgPath, alt) => {
-  const link = document.createElement("a");
-  const img = imgElement(alt, imgPath);
-  link.setAttribute("href", url);
-  link.appendChild(img);
+export const createClickableIMG = (hrefURL, srcURL, altText) => {
+  const img = createElement("img", { src: srcURL, alt: altText });
+  const link = createElement("a", { href: hrefURL });
+  link.append(img);
   return link;
 };
