5eb2e9db64e7568ff1d0364cb727bd869e3b9956
veerleprins <veerleprins@hotmail.com>
2021-02-28 18:03:47 +0100

    Changed append to appendChild

--- src/scripts/components/molecules/img-link.js
+++ src/scripts/components/molecules/img-link.js
@@ -4,6 +4,6 @@ import { createElement } from "../atoms/element";
 export const createClickableIMG = (hrefURL, srcURL, altText) => {
   const img = createElement("img", { src: srcURL, alt: altText });
   const link = createElement("a", { href: hrefURL });
-  link.append(img);
+  link.appendChild(img);
   return link;
 };
