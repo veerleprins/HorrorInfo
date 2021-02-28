e371db394cb9abffb5303516a77254b4def19f8c
veerleprins <veerleprins@hotmail.com>
2021-03-01 00:28:49 +0100

    Changed function names

--- src/scripts/components/organisms/header.js
+++ src/scripts/components/organisms/header.js
@@ -1,11 +1,11 @@
 // Components
-import { inputElement } from "../molecules/input";
+import { createInput } from "../molecules/input";
 import { createElement } from "../atoms/element";
 
 export const createHeader = () => {
   // This function builds the header of the home page:
   const header = createElement("header");
-  const article = inputElement(
+  const article = createInput(
     "h1",
     "What horror are you looking for?",
     "Search movies..."
