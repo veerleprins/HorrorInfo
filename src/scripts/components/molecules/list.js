53f74f05c258e747cd9754287805f2c3ef07b753
veerleprins <veerleprins@hotmail.com>
2021-03-01 00:24:04 +0100

    Changed let to const & imported helper file

--- src/scripts/components/molecules/list.js
+++ src/scripts/components/molecules/list.js
@@ -1,26 +1,29 @@
 // Internals
 import { posterPath } from "../../config/config";
 
+// Modules
+import { appendToElement } from "../../modules/helpers/append";
+
 // Components
 import { createElement } from "../atoms/element";
 import { createClickableIMG } from "./img-link";
 
 export const createList = (dataArray) => {
   const ul = createElement("ul");
+
   dataArray.forEach((item) => {
     // Creating elements
-    let li = createElement("li");
-    let title = createElement("h3", {}, item.title);
-    let link = createClickableIMG(
+    const li = createElement("li");
+    const title = createElement("h3", {}, item.title);
+    const link = createClickableIMG(
       `/movie/${item.id}`,
       `${posterPath}${item.poster}`,
       `Poster of the movie: ${item.title}`
     );
 
     // Appending elements
-    li.append(title);
-    li.append(link);
-    ul.append(li);
+    appendToElement([title, link], li);
+    appendToElement([li], ul);
   });
   return ul;
 };
